e6130432b7dbfb37832c9896fd9b1f6f73621327
Łukasz Pałys <lukasz.palys.it@gmail.com>
2018-03-27 00:57:51 +0200

    Tests with config from a file

--- src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
+++ src/main/java/guru/springframework/spring5webapp/Spring5webappApplication.java
@@ -5,6 +5,7 @@ import guru.springframework.spring5webapp.controllers.MyController;
 import guru.springframework.spring5webapp.controllers.PropertyInjectedController;
 import guru.springframework.spring5webapp.controllers.SetterInjectedController;
 import guru.springframework.spring5webapp.database.DatabaseSource;
+import guru.springframework.spring5webapp.database.JMSSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -18,11 +19,12 @@ public class Spring5webappApplication {
 		MyController controller = (MyController) ctx.getBean("myController");
 		//controller.hello();
 		DatabaseSource databaseSource =  ctx.getBean(DatabaseSource.class);
+		JMSSource jmsSource =  ctx.getBean(JMSSource.class);
 
 		System.out.println(controller.hello());
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
 		System.out.println(databaseSource.getUserName());
-
+		System.out.println(jmsSource.getUserName());
 	}
 }
--- src/main/java/guru/springframework/spring5webapp/config/DatabaseSourceConfig.java
+++ src/main/java/guru/springframework/spring5webapp/config/DatabaseSourceConfig.java
@@ -1,20 +1,32 @@
 package guru.springframework.spring5webapp.config;
 
 import guru.springframework.spring5webapp.database.DatabaseSource;
+import guru.springframework.spring5webapp.database.JMSSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+//@PropertySource({"classpath:datasource.properties","classpath:jms.properties"})
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class DatabaseSourceConfig {
     @Value("${guru.username}")
     String userName;
 
     @Value("${guru.password}")
     String password;
+
+    @Value("${jms.username}")
+    String jmsUserName;
+
+    @Value("${jms.password}")
+    String jmsPassword;
 
     @Value("${guru.dbUrl}")
     String dbUrl;
@@ -27,6 +39,15 @@ public class DatabaseSourceConfig {
         databaseSource.setPassword(dbUrl);
 
         return databaseSource;
+    }
+
+    @Bean
+    public JMSSource jmsSource() {
+        JMSSource jmsSource = new JMSSource()   ;
+        jmsSource.setUserName(jmsUserName);
+        jmsSource.setPassword(jmsPassword);
+
+        return jmsSource;
     }
 
     @Bean
